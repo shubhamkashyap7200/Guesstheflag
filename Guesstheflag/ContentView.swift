16021f555baeb80da435423fc100186b2ef1bfc0
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-12 11:43:26 +0530

    More Progress made

--- Guesstheflag/ContentView.swift
+++ Guesstheflag/ContentView.swift
@@ -16,6 +16,15 @@ struct ContentView: View {
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
     
+    struct FlagImageView: View {
+        var imgStr: String
+        var body: some View {
+            Image(imgStr)
+                .clipShape(.capsule)
+                .shadow(radius: 5)
+        }
+    }
+    
     // MARK: - Views
     var body: some View {
         ZStack {
@@ -49,9 +58,7 @@ struct ContentView: View {
                             Button {
                                 flagTapped(number)
                             } label: {
-                                Image(countries[number])
-                                    .clipShape(.capsule)
-                                    .shadow(radius: 5)
+                                FlagImageView(imgStr: countries[number])
                             }
                         }
                     }
