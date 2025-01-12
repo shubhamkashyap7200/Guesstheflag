49391c46f9236c06e6e998b67414effed693f45b
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-12 08:02:22 +0530

    Initial Commit

---
+++ Guesstheflag/GuesstheflagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuesstheflagApp.swift
+//  Guesstheflag
+//
+//  Created by Shubham on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct GuesstheflagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
